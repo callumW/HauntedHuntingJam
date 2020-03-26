818c32d030b7aca18f4ba35b158a1d73cdc394a2
Callum Wilson <calwilson@live.co.uk>
2020-03-26 21:41:26 +0000

    Adding initial unreal engine project

---
+++ Source/HauntedHuntingJam/HauntedHuntingJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HauntedHuntingJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHauntedHuntingJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHauntedHuntingJamGameMode();
+};
+
+
+
