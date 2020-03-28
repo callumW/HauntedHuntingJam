416d8fc4e9ed8de21d810b1bddf5c9d457e36d66
Callum Wilson <calwilson@live.co.uk>
2020-03-28 20:26:17 +0000

    Skeleton of Forest Generation

---
+++ Source/HauntedHuntingJam/ForestBuilder.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ForestGenerator.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ForestBuilder.generated.h"
+
+UCLASS()
+class HAUNTEDHUNTINGJAM_API AForestBuilder : public AActor
+{
+	GENERATED_BODY()
+
+	ForestGenerator m_generator;
+
+	void OutputToBMP(std::vector<map_chunk_t> const& map, FString filename_base);
+	void OutputToBMP(map_chunk_t const& map, FString filename);
+
+public:
+	// Sets default values for this actor's properties
+	AForestBuilder();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/HauntedHuntingJam/ForestGenerator.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <vector>
+
+#include "CoreMinimal.h"
+#include "Layout/SlateRect.h"	// For FSlateRect
+
+typedef struct tree {
+	FVector location;
+	FRotator rotation;
+} tree_t;
+
+typedef struct map_chunk {
+	std::vector<tree_t> trees;
+	FSlateRect rect;
+} map_chunk_t;
+
+/**
+ *	Generate forest data
+ */
+class HAUNTEDHUNTINGJAM_API ForestGenerator
+{
+public:
+	ForestGenerator();
+	~ForestGenerator();
+
+	std::vector<map_chunk_t> GenerateForest();
+};
