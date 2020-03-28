ff97840e03d24b672bf5a508edf8464b9b7df514
Callum Wilson <calwilson@live.co.uk>
2020-03-28 22:49:41 +0000

    Randomise rotation of trees

--- Source/HauntedHuntingJam/ForestGenerator.cpp
+++ Source/HauntedHuntingJam/ForestGenerator.cpp
@@ -24,17 +24,20 @@ std::vector<map_chunk_t> ForestGenerator::GenerateForest()
 
     std::uniform_real_distribution<float> x_dist(chunk.rect.Left, chunk.rect.Right);
     std::uniform_real_distribution<float> y_dist(chunk.rect.Top, chunk.rect.Bottom);
+    std::uniform_real_distribution<float> rot_dist(0.0f, 360.0f);
 
     std::mt19937 x_engine(m_random_engine());
     std::mt19937 y_engine(m_random_engine());
+    std::mt19937 rot_engine(m_random_engine());
 
     auto get_x = std::bind(x_dist, x_engine);
     auto get_y = std::bind(y_dist, m_random_engine);
+    auto get_rotation = std::bind(rot_dist, rot_engine);
 
     size_t const num_points = 100;
 
     for (size_t i = 0; i < num_points; i++) {
-        tree_t tree = {FVector(get_x(), get_y(), 0.0f), FRotator(0.0f)};
+        tree_t tree = {FVector(get_x(), get_y(), 0.0f), FRotator(0.0, get_rotation(), 0.0f)};
         chunk.trees.push_back(tree);
     }
 
