bbf2b04e50c2358ac5764d8bc4aa4a5ed7bd614c
Callum Wilson <calwilson@live.co.uk>
2020-03-28 20:26:17 +0000

    Skeleton of Forest Generation

---
+++ Source/HauntedHuntingJam/ForestBuilder.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include <array>
+
+#include "Misc/FileHelper.h"
+#include "Math/Color.h"
+
+#include "ForestBuilder.h"
+
+// Sets default values
+AForestBuilder::AForestBuilder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AForestBuilder::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto map = m_generator.GenerateForest();
+
+}
+
+// Called every frame
+void AForestBuilder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AForestBuilder::OutputToBMP(std::vector<map_chunk_t> const& map, FString filename_base)
+{
+	for (auto & chunk : map) {
+		FString filename = filename_base;
+		filename.Appendf(TEXT("%%f-%f.bmp"), chunk.rect.Left, chunk.rect.Bottom);
+		OutputToBMP(chunk, filename);
+	}
+}
+
+void AForestBuilder::OutputToBMP(map_chunk_t const& map, FString filename)
+{
+	auto const map_size = map.rect.GetSize();
+
+	int32 width = static_cast<int32>(map_size.X);
+	int32 height = static_cast<int32>(map_size.Y);
+
+	size_t const num_pixels = static_cast<size_t>(width * height);
+
+	std::vector<struct FColor> pixel_data = {num_pixels, FColor(0)};
+
+	IFileManager& file_manager = IFileManager::Get();
+	if (!FFileHelper::CreateBitmap(*filename, width, height, pixel_data.data(),
+		nullptr, &file_manager, nullptr, false)) {
+		UE_LOG(LogTemp, Display, TEXT("Failed to create bitmap"));
+	}
+}
---
+++ Source/HauntedHuntingJam/ForestGenerator.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ForestGenerator.h"
+
+ForestGenerator::ForestGenerator()
+{
+}
+
+ForestGenerator::~ForestGenerator()
+{
+}
+
+std::vector<map_chunk_t> ForestGenerator::GenerateForest()
+{
+    std::vector<map_chunk_t> map;
+    return map;
+}
