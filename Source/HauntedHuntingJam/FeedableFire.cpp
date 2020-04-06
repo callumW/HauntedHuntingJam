dcec0807c8ddad93bbc878e93251d3b43ae96853
Callum Wilson <calwilson@live.co.uk>
2020-04-06 12:30:22 +0100

    Add Feed function & fixed component resetting on compilation issue #16

--- Source/HauntedHuntingJam/FeedableFire.cpp
+++ Source/HauntedHuntingJam/FeedableFire.cpp
@@ -10,6 +10,10 @@ AFeedableFire::AFeedableFire()
 	PrimaryActorTick.bCanEverTick = true;
 
 	if (!RootComponent) RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("fire_root"));
+
+	if (container_mesh) container_mesh->SetupAttachment(RootComponent);
+
+	if (fire_particle_system) fire_particle_system->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -23,4 +27,19 @@ void AFeedableFire::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+int32 AFeedableFire::Feed(int32 wood_count)
+{
+	float incoming_fuel = static_cast<float>(wood_count);
+
+	float used_fuel = max_fuel_count - fuel_count;
+	if (used_fuel >= 1.0f) { 	//Verify we can add a whole amount of fuel
+		// Calculate used amount of fuel
+		used_fuel = FMath::RoundToZero(used_fuel);
+		fuel_count += used_fuel;
+
+		return static_cast<int32>(used_fuel);
+	}
+	return 0;
 }
--- Source/HauntedHuntingJam/FeedableFire.h
+++ Source/HauntedHuntingJam/FeedableFire.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "Particles/ParticleSystemComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "FeedableFire.generated.h"
@@ -12,18 +14,30 @@ class HAUNTEDHUNTINGJAM_API AFeedableFire : public AActor
 private:
 	GENERATED_BODY()
 
-	UPROPERTY(EditAnywhere)
-	class UParticleSystemComponent* fire_particle_system;
-
-	UPROPERTY(EditAnywhere)
-	class UStaticMeshComponent* container_mesh;
-
 public:
+	UPROPERTY(EditAnywhere)
+	UParticleSystemComponent* fire_particle_system = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* container_mesh = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="Gameplay")
+	float fuel_burn_rate = 1.0f;
+
+	UPROPERTY(EditAnywhere)
+	float fuel_count = 20;
+
+	UPROPERTY(EditAnywhere)
+	float max_fuel_count = 20;
+
 	// Sets default values for this actor's properties
 	AFeedableFire();
 
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	// return true if feed was successful (e.g. fire is still alive)
+	int32 Feed(int32 wood_count);
 
 protected:
 	// Called when the game starts or when spawned
